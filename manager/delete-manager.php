f5241dd48ec43640b0f36feadbaea7341fc0a194
Ethan <epaleyron@edenschool.fr>
2023-12-07 13:45:34 +0100

    fin faire une retouche de scss

--- manager/delete-manager.php
+++ manager/delete-manager.php
@@ -2,44 +2,45 @@
 session_start();
 try {
     include("../manager/connexion-base.php");
-    $sql = "SELECT LABEL_UTILISATEUR_ARTICLE FROM `articles` WHERE ID_ARTICLE = ".$_GET["id_article"];
+    $sql = "SELECT LABEL_UTILISATEUR_ARTICLE FROM `articles` WHERE ID_ARTICLE = " . $_GET["id_article"];
     $result = $base->query($sql);
     $e = $result->fetch();
-    if($_SESSION["id"] != $e["LABEL_UTILISATEUR_ARTICLE"]) {
+    if ($_SESSION["id"] != $e["LABEL_UTILISATEUR_ARTICLE"]) {
         header("Location: http://localhost/article-pdo/pages/index.php");
+    } else {
+        try {
+            include("../manager/connexion-base.php");
+            $sql = "DELETE FROM `sou-commentaires` WHERE LABEL_COMMENTAIRE = :id_sou_commentaires";
+            $deleteSouCommentaires = $base->prepare($sql);
+            $deleteSouCommentaires->execute(array("id_sou_commentaires" => $_POST["id_sou_commentaires"]));
+            $deleteSouCommentaires->closeCursor();
+        } catch (Exception $e) {
+            throw new InvalidArgumentException($e->getMessage());
+        }
+        try {
+            include("../manager/connexion-base.php");
+            $sql = "DELETE FROM `commentaires` WHERE LABEL_ARTICLE = :id_article";
+            $deleteCommentaires = $base->prepare($sql);
+            $deleteCommentaires->execute(array("id_article" => $_GET["id_article"]));
+            $deleteCommentaires->closeCursor();
+        } catch (Exception $e) {
+            throw new InvalidArgumentException($e->getMessage());
+        }
+        try {
+            include("../manager/connexion-base.php");
+            $sql = "DELETE FROM `articles` WHERE ID_ARTICLE = :id_article";
+            $file_to_delete = "../pages/files/" . $_POST["image_delete"];
+            if (file_exists($file_to_delete)) {
+                unlink($file_to_delete);
+            }
+            $deleteArticle = $base->prepare($sql);
+            $deleteArticle->execute(array("id_article" => $_GET["id_article"]));
+            $deleteArticle->closeCursor();
+            header("Location: http://localhost/article-pdo/pages/index.php");
+        } catch (Exception $e) {
+            throw new InvalidArgumentException($e->getMessage());
+        }
     }
-} catch (Exception $e) {
-    throw new InvalidArgumentException($e->getMessage());
-}
-try {
-    include("../manager/connexion-base.php");
-    $sql = "DELETE FROM `sou-commentaires` WHERE LABEL_COMMENTAIRE = :id_sou_commentaires";
-    $deleteSouCommentaires = $base->prepare($sql);
-    $deleteSouCommentaires->execute(array("id_sou_commentaires" => $_POST["id_sou_commentaires"]));
-    $deleteSouCommentaires->closeCursor();
-} catch (Exception $e) {
-    throw new InvalidArgumentException($e->getMessage());
-}
-try {
-    include("../manager/connexion-base.php");
-    $sql = "DELETE FROM `commentaires` WHERE LABEL_ARTICLE = :id_article";
-    $deleteCommentaires = $base->prepare($sql);
-    $deleteCommentaires->execute(array("id_article" => $_GET["id_article"]));
-    $deleteCommentaires->closeCursor();
-} catch (Exception $e) {
-    throw new InvalidArgumentException($e->getMessage());
-}
-try {
-    include("../manager/connexion-base.php");
-    $sql = "DELETE FROM `articles` WHERE ID_ARTICLE = :id_article";
-    $file_to_delete = "files/".$_POST["image_delete"];
-    if(file_exists($file_to_delete)) {
-        unlink($file_to_delete);
-    }
-    $deleteArticle = $base->prepare($sql);
-    $deleteArticle->execute(array("id_article" => $_GET["id_article"]));
-    $deleteArticle->closeCursor();
-    header("Location: http://localhost/article-pdo/pages/index.php");
 } catch (Exception $e) {
     throw new InvalidArgumentException($e->getMessage());
 }
--- pages/index.php
+++ pages/index.php
@@ -18,7 +18,7 @@
         </a>
         <ul>
             <?php
-            if(isset($_SESSION["id"])) {
+            if (isset($_SESSION["id"])) {
                 echo '<li><a href="../manager/logout-manager.php">Deconnexion</a></li>';
             } else {
                 echo '<li><a href="create-account.php">Créer un compte</a></li>
@@ -27,14 +27,18 @@
             ?>
         </ul>
         <?php
-        if(isset($_SESSION["id"])) {
-            echo "<p>Connecter en tant que ".$_SESSION["user_name"]."</p>";
+        if (isset($_SESSION["id"])) {
+            echo "<p>Connecter en tant que " . $_SESSION["user_name"] . "</p>";
         }
         ?>
     </nav>
 
     <h2>Tout les articles</h2>
-    <a href="create-article.php">Créer un nouvel article</a>
+    <?php
+    if (isset($_SESSION["id"])) {
+        echo '<a href="create-article.php">Créer un nouvel article</a>';
+    }
+    ?>
 
     <div class="articles">
         <?php
@@ -42,25 +46,25 @@
             include("../manager/connexion-base.php");
             $sql = "SELECT * FROM `articles` ORDER BY DATE_HEURE_ARTICLE";
             $result = $base->query($sql);
-            while($e = $result->fetch()) {
+            while ($e = $result->fetch()) {
                 $datetime = new DateTime($e["DATE_HEURE_ARTICLE"]);
                 echo '<article>
-                <h2>'.$e["NOM_ARTICLE"].'</h2>
-                <time datetime="'.$datetime->format("d-m-Y H:i:s").'">'.$datetime->format("d-m-Y H:i:s").'</time>
-                <img src="files/'.$e["IMAGE_ARTICLE"].'" alt="'.$e["IMAGE_ARTICLE"].'">
-                <p>'.$e["DESCRIPTION_ARTICLE"].'</p>';
-                if(isset($_SESSION["id"])) {
-                    if($_SESSION["id"] == $e["LABEL_UTILISATEUR_ARTICLE"]) {
-                        echo '<a href="update-article.php?id_article='.$e["ID_ARTICLE"].'">Modifier</a>
-                        <form action="../manager/delete-manager.php?id_article='.$e["ID_ARTICLE"].'" method="post" enctype="multipart/form-data">
-                        <input type="hidden" name="image_delete" value="'.$e["IMAGE_ARTICLE"].'">';
+                <h2>' . $e["NOM_ARTICLE"] . '</h2>
+                <time datetime="' . $datetime->format("d-m-Y H:i:s") . '">' . $datetime->format("d-m-Y H:i:s") . '</time>
+                <img src="files/' . $e["IMAGE_ARTICLE"] . '" alt="' . $e["IMAGE_ARTICLE"] . '">
+                <p>' . $e["DESCRIPTION_ARTICLE"] . '</p>';
+                if (isset($_SESSION["id"])) {
+                    if ($_SESSION["id"] == $e["LABEL_UTILISATEUR_ARTICLE"]) {
+                        echo '<a href="update-article.php?id_article=' . $e["ID_ARTICLE"] . '">Modifier</a>
+                        <form action="../manager/delete-manager.php?id_article=' . $e["ID_ARTICLE"] . '" method="post" enctype="multipart/form-data">
+                        <input type="hidden" name="image_delete" value="' . $e["IMAGE_ARTICLE"] . '">';
                         try {
                             include("../manager/connexion-base.php");
-                            $sql = "SELECT ID_COMMENTAIRE FROM `commentaires` WHERE LABEL_ARTICLE = ".$e["ID_ARTICLE"];
+                            $sql = "SELECT ID_COMMENTAIRE FROM `commentaires` WHERE LABEL_ARTICLE = " . $e["ID_ARTICLE"];
                             $idCommentaires = $base->query($sql);
                             $commentaires = $idCommentaires->fetch();
-                            if($commentaires != "") {
-                                echo '<input type="hidden" name="id_sou_commentaires" value="'.$commentaires["ID_COMMENTAIRE"].'">';
+                            if ($commentaires != "") {
+                                echo '<input type="hidden" name="id_sou_commentaires" value="' . $commentaires["ID_COMMENTAIRE"] . '">';
                             }
                         } catch (Exception $e) {
                             throw new InvalidArgumentException($e->getMessage());
@@ -72,7 +76,7 @@
                 echo '<form action="../manager/commentaire-manager.php" method="post" enctype="multipart/form-data">
                     <label for="comment">Ajouter un commentaire:</label>
                     <input type="text" name="comment" id="comment">
-                    <input type="hidden" name="id_article_comment" value="'.$e["ID_ARTICLE"].'">
+                    <input type="hidden" name="id_article_comment" value="' . $e["ID_ARTICLE"] . '">
                     <input type="submit" value="Envoyer">
                 </form>';
                 echo '<ul>';
@@ -81,12 +85,12 @@
                     $sql = "SELECT * FROM `commentaires` WHERE LABEL_ARTICLE = :label_article ORDER BY DATE_HEURS_COMMENTAIRE";
                     $commentaires = $base->prepare($sql);
                     $commentaires->execute(array("label_article" => $e["ID_ARTICLE"]));
-                    while($e = $commentaires->fetch()) {
-                        echo '<li>'.$e["COMMENTAIRE_ARTICLE"].'
+                    while ($e = $commentaires->fetch()) {
+                        echo '<li>' . $e["COMMENTAIRE_ARTICLE"] . '
                         <form action="../manager/sou-commentaire-manager.php" method="post" enctype="multipart/form-data">
                             <label for="sou_comment">Ajouter un sou-commentaire:</label>
                             <input type="text" name="sou_comment" id="sou_comment">
-                            <input type="hidden" name="id_comment_sou_comment" value="'.$e["ID_COMMENTAIRE"].'">
+                            <input type="hidden" name="id_comment_sou_comment" value="' . $e["ID_COMMENTAIRE"] . '">
                             <input type="submit" value="Envoyer">
                         </form>
                         <ul>';
@@ -95,8 +99,8 @@
                             $sql = "SELECT * FROM `sou-commentaires` WHERE LABEL_COMMENTAIRE = :label_commentaire ORDER BY DATE_HEURE_SOU_COMMENTAIRE";
                             $souCommentaires = $base->prepare($sql);
                             $souCommentaires->execute(array("label_commentaire" => $e["ID_COMMENTAIRE"]));
-                            while($e = $souCommentaires->fetch()) {
-                                echo '<li>'.$e["SOU_COMMENTAIRE"].'</li>';
+                            while ($e = $souCommentaires->fetch()) {
+                                echo '<li>' . $e["SOU_COMMENTAIRE"] . '</li>';
                             }
                         } catch (Exception $e) {
                             throw new InvalidArgumentException($e->getMessage());
